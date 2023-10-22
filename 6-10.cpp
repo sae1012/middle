606295208d5035a4866efae49f94ab6bf991e93e
grigory karpunin <143996987+sae1012@users.noreply.github.com>
2023-10-22 12:26:50 +0300

    Update 6-10.cpp

--- 6-10.cpp
+++ 6-10.cpp
@@ -32,10 +32,20 @@ int itc_min_num(long long number){
 int itc_rev_num(long long number){
     int digit, rebmun, sum = 0;
     rebmun = 0;
-    while(number){
-        digit = number % 10;
-        rebmun = rebmun * 10 + digit;
-        number /= 10;
+    if(number < 0){
+        number = number * (-1);
+        while(number){
+            digit = number % 10;
+            rebmun = rebmun * 10 + digit;
+            number /= 10;
+        }
+    }
+    else{
+        while(number){
+            digit = number % 10;
+            rebmun = rebmun * 10 + digit;
+            number /= 10;
+        }
     }
     while(rebmun){
         sum += 1;
@@ -47,6 +57,9 @@ int itc_rev_num(long long number){
 
 int itc_null_count(long long number){
     int null = 0;
+    if(number == 0){
+        return 1;
+    }
     if(number < 0){
         number *= -1;
         while(number > 0){
